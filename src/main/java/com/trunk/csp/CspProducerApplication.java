7bdd7e511c4f7c85b50e24c7a68c45095b8d2e80
shahin-trunk <shahin@ourtrunk.com>
2020-01-23 15:08:29 +0400

    Initial commit

---
+++ src/main/java/com/trunk/csp/CspProducerApplication.java
@@ -0,0 +1,13 @@
+package com.trunk.csp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trunk/csp/CspProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunk.csp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
